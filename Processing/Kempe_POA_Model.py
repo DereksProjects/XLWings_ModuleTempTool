ffff4a292b594126bc56cf1510584c8a14c57614
Holsapple <dholsapp@nrel.gov>
2019-10-23 12:58:41 -0600

    Created a function to be used with proicessing to reduce amount of code in Level_1_Dataframe_to_Pickle.py

--- Processing/Kempe_POA_Model.py
+++ Processing/Kempe_POA_Model.py
@@ -98,19 +98,114 @@ def universalTimeCorrected(dateTimeObj, hoursAheadOrBehind):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def cleanedFrame( firstRow_summary_df, fileNames , index ):    
+    raw_df = pd.read_pickle( path + '\\Pandas_Pickle_DataFrames\\Pickle_RawData\\' + fileNames[index])
+    
+    level_1_df = raw_df.loc[:,['Date (MM/DD/YYYY)', 
+                               'Time (HH:MM)',
+                               'Albedo',
+                               'Global horizontal irradiance',
+                               'Direct normal irradiance',
+                               'Diffuse horizontal irradiance',
+                               'Dry-bulb temperature',
+                               'Dew-point temperature',
+                               'Relative humidity',
+                               'Station pressure',
+                               'Wind direction',
+                               'Wind speed',
+                               'Total sky cover',
+                               'Precipitable water',
+                               'Liquid percipitation depth']]
+    ###############
+    # Correct data to pull out metrics, create sub data frames for processing
+            # Predicted Albedo needs to be .2 if data is not provided
+            # Total sky coverage is currently in tenths, Needs to be in okta(1/8)
+    
+    ###############
+            
+    #ALBEDO CORRECTION, subframe not needed. IWEC data all contained NA data for Albedo    
+    
+    # If Albedo contains NA data then replace NA with .2 (Defualt Value for Albedo)
+    level_1_df.Albedo = level_1_df.Albedo.fillna(.2)
+    
+    # Correcting the Albedo
+    #If the Albedo falls below 0 then correect the Albedo to 0.133
+    #Lambda starts at the first element of the row being named "x" and processes until the last element of the dataframe
+    level_1_df['Corrected Albedo'] = level_1_df.Albedo.apply(lambda x: 0.133 if x <= 0 or x >= 100 else x)
+    
+    # TOTAL SKY COVERAGE, sub data frame needed to change sky coverage scale from tenths(1/10) to okta(1/8)
+                        # Okta frame will be used to calculate the estimated yearly dew yield
+    level_1_df['Total sky cover(okta)'] = (level_1_df['Total sky cover'].astype(float) * 8) / 10
+    
+    
+    
+    
+    
+    ################         
+    #Create Date Time objects as columns, this includes finding Solar Time
+    ################       
+    #Create a data frame to store a combined string frame of Date column and Time column
+    DateTimeStrings = level_1_df['Date (MM/DD/YYYY)'].str.cat(level_1_df['Time (HH:MM)'],sep=" ")
+    
+    # Create a new column of the level_1_df named Local Date Time
+    # Use the DateTimeStrings frame to convert to a date time objects
+    # Store the new date time objects into the Local Date Time column
+    #Note: The my_to_datetime() will correct 24:00 to the next day at 0:00
+    level_1_df['Local Date Time'] = DateTimeStrings.apply(lambda x: my_to_datetime(x))
+    
+    
+    #Correct for Universal Time
+    # From the first Row summary frame pull out the number of hours by 
+    #    which local standard time is ahead or behind Universal Time ( + or -)
+    hoursAheadOrBehind = float(firstRow_summary_df.iloc[83]['Site time zone (Universal time + or -)'])
+    
+    # Correct the datetime object to universal time
+    # Use the helper method universalTimeCorrected() to process each local 
+    #     date time object
+    # Create a new column in the level_1_df to store the Universal Date time object
+    level_1_df['Universal Date Time'] = level_1_df['Local Date Time'].apply(lambda x: universalTimeCorrected(x, hoursAheadOrBehind))
+    
+    
+    #Calculate the Local Solar time
+    # Use the localTimeToSolarTime() helper method
+    # Create a new column in the level_1_df to store the Universal Date time object
+    level_1_df['Local Solar Time'] = level_1_df.apply(lambda x: localTimeToSolarTime( longitude , hoursAheadOrBehind , x['Local Date Time']), axis=1)
+     
+    #Create another column of the hourly numeric Local Solar Time
+    level_1_df['Hourly Local Solar Time'] = level_1_df['Local Solar Time'].apply(lambda x: x.hour + (x.minute/60))
+    #We also need the hourly local time in minutes
+    level_1_df['Minutes Local Solar Time'] = level_1_df['Local Solar Time'].apply(lambda x: x.hour*60  + x.minute)
+    ################################################################
+    # Slight Clean up the frame before processing 
+    
+    # Drop the old Date and Time (Strings) columns
+    level_1_df = level_1_df.drop(columns=['Date (MM/DD/YYYY)', 'Time (HH:MM)' ])
+    
+    # Re index the column headings in a more organized format 
+    level_1_df = level_1_df.reindex(columns = ['Local Date Time', 
+                                               'Local Solar Time',
+                                               'Hourly Local Solar Time',
+                                               'Minutes Local Solar Time',
+                                               'Universal Date Time',
+                                               'Albedo', 
+                                               'Corrected Albedo',
+                                               'Global horizontal irradiance',
+                                               'Direct normal irradiance', 
+                                               'Diffuse horizontal irradiance',
+                                               'Dry-bulb temperature',
+                                               'Dew-point temperature',
+                                               'Relative humidity',
+                                               'Station pressure',
+                                               'Wind direction',
+                                               'Wind speed',
+                                               'Total sky cover',
+                                               'Total sky cover(okta)',
+                                               'Precipitable water',
+                                               'Liquid percipitation depth'
+                                               ])
+    
+    
+    return level_1_df  
 
 
 
@@ -139,6 +234,7 @@ latitude = float(firstRow_summary_df.loc[83]['Site latitude'])
 longitude = float(firstRow_summary_df.loc[83]['Site longitude']) 
 
 
+test = cleanedFrame( firstRow_summary_df , fileNames , 83 )
 
 
 
@@ -151,120 +247,7 @@ longitude = float(firstRow_summary_df.loc[83]['Site longitude'])
 
 
 
-
-
-
-raw_df = pd.read_pickle( path + '\\Pandas_Pickle_DataFrames\\Pickle_RawData\\' + fileNames[83])
-
-level_1_df = raw_df.loc[:,['Date (MM/DD/YYYY)', 
-                           'Time (HH:MM)',
-                           'Albedo',
-                           'Global horizontal irradiance',
-                           'Direct normal irradiance',
-                           'Diffuse horizontal irradiance',
-                           'Dry-bulb temperature',
-                           'Dew-point temperature',
-                           'Relative humidity',
-                           'Station pressure',
-                           'Wind direction',
-                           'Wind speed',
-                           'Total sky cover',
-                           'Precipitable water',
-                           'Liquid percipitation depth']]
-###############
-# Correct data to pull out metrics, create sub data frames for processing
-        # Predicted Albedo needs to be .2 if data is not provided
-        # Total sky coverage is currently in tenths, Needs to be in okta(1/8)
-
-###############
-        
-#ALBEDO CORRECTION, subframe not needed. IWEC data all contained NA data for Albedo    
-
-# If Albedo contains NA data then replace NA with .2 (Defualt Value for Albedo)
-level_1_df.Albedo = level_1_df.Albedo.fillna(.2)
-
-# Correcting the Albedo
-#If the Albedo falls below 0 then correect the Albedo to 0.133
-#Lambda starts at the first element of the row being named "x" and processes until the last element of the dataframe
-level_1_df['Corrected Albedo'] = level_1_df.Albedo.apply(lambda x: 0.133 if x <= 0 or x >= 100 else x)
-
-# TOTAL SKY COVERAGE, sub data frame needed to change sky coverage scale from tenths(1/10) to okta(1/8)
-                    # Okta frame will be used to calculate the estimated yearly dew yield
-level_1_df['Total sky cover(okta)'] = (level_1_df['Total sky cover'].astype(float) * 8) / 10
-
-
-
-
-
-################         
-#Create Date Time objects as columns, this includes finding Solar Time
-################       
-#Create a data frame to store a combined string frame of Date column and Time column
-DateTimeStrings = level_1_df['Date (MM/DD/YYYY)'].str.cat(level_1_df['Time (HH:MM)'],sep=" ")
-
-# Create a new column of the level_1_df named Local Date Time
-# Use the DateTimeStrings frame to convert to a date time objects
-# Store the new date time objects into the Local Date Time column
-#Note: The my_to_datetime() will correct 24:00 to the next day at 0:00
-level_1_df['Local Date Time'] = DateTimeStrings.apply(lambda x: my_to_datetime(x))
-
-
-#Correct for Universal Time
-# From the first Row summary frame pull out the number of hours by 
-#    which local standard time is ahead or behind Universal Time ( + or -)
-hoursAheadOrBehind = float(firstRow_summary_df.iloc[83]['Site time zone (Universal time + or -)'])
-
-# Correct the datetime object to universal time
-# Use the helper method universalTimeCorrected() to process each local 
-#     date time object
-# Create a new column in the level_1_df to store the Universal Date time object
-level_1_df['Universal Date Time'] = level_1_df['Local Date Time'].apply(lambda x: universalTimeCorrected(x, hoursAheadOrBehind))
-
-
-#Calculate the Local Solar time
-# Use the localTimeToSolarTime() helper method
-# Create a new column in the level_1_df to store the Universal Date time object
-level_1_df['Local Solar Time'] = level_1_df.apply(lambda x: localTimeToSolarTime( longitude , hoursAheadOrBehind , x['Local Date Time']), axis=1)
- 
-#Create another column of the hourly numeric Local Solar Time
-level_1_df['Hourly Local Solar Time'] = level_1_df['Local Solar Time'].apply(lambda x: x.hour + (x.minute/60))
-#We also need the hourly local time in minutes
-level_1_df['Minutes Local Solar Time'] = level_1_df['Local Solar Time'].apply(lambda x: x.hour*60  + x.minute)
-################################################################
-# Slight Clean up the frame before processing 
-
-# Drop the old Date and Time (Strings) columns
-level_1_df = level_1_df.drop(columns=['Date (MM/DD/YYYY)', 'Time (HH:MM)' ])
-
-# Re index the column headings in a more organized format 
-level_1_df = level_1_df.reindex(columns = ['Local Date Time', 
-                                           'Local Solar Time',
-                                           'Hourly Local Solar Time',
-                                           'Minutes Local Solar Time',
-                                           'Universal Date Time',
-                                           'Albedo', 
-                                           'Corrected Albedo',
-                                           'Global horizontal irradiance',
-                                           'Direct normal irradiance', 
-                                           'Diffuse horizontal irradiance',
-                                           'Dry-bulb temperature',
-                                           'Dew-point temperature',
-                                           'Relative humidity',
-                                           'Station pressure',
-                                           'Wind direction',
-                                           'Wind speed',
-                                           'Total sky cover',
-                                           'Total sky cover(okta)',
-                                           'Precipitable water',
-                                           'Liquid percipitation depth'
-                                           ])
-
-
-
-
-
-
-
+    
 
 
 
